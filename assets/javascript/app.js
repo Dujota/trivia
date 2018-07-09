efaa1fac0556a42dfcf070d45115af3020557361
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-07-08 20:00:04 -0400

    created the question object that i will use to work with

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+  
+  const trivia = [{
+    queston: "Which animal is a bird?",
+    options: ["doberman", "lion", "anaconda", "blue jay"],
+    answer: "blue jay"
+  }]
+
+
+
+
+
+});
